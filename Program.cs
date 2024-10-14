8e5b9fbde6997c94876332fcf18b20d06166fddb
Seth-Woods-Student <sewoae4@student.oaisd.org>
2024-10-14 13:42:18 +0000

    done

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program {
+    static void Main(string[] args) {
+        float res;
+        float num1 = float.Parse(Console.ReadLine());
+        string mid = Console.ReadLine();
+        float num2 = float.Parse(Console.ReadLine());
+
+        switch (mid) {
+            case "+":
+            res = num1 + num2;
+            Console.WriteLine( num1  + " + " + num2 + " = " + res);
+            break;
+            case "-":
+            res = num1 - num2;
+            Console.WriteLine( num1  + " - " + num2 + " = " + res);
+            break;
+            case "*":
+            res = num1 * num2;
+            Console.WriteLine( num1  + " * " + num2 + " = " + res);
+            break;
+            case "/":
+            res = num1 / num2;
+            Console.WriteLine( num1  +  " / " + num2 + " = " + res);
+            break;
+            case "%":
+            res = num1 / num2;
+            Console.WriteLine( num1  + " % " + num2 + " = " + res);
+            break;
+        }
+
+    }
+}
